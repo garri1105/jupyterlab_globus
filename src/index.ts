697dcb1021b06d058830260a2660209b76cab571
Juan David Garrido <juandagarrido@hotmail.com>
2018-05-24 20:51:56 -0500

    Added utility functions

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,28 @@
-import {
-    JupyterLab, JupyterLabPlugin
-} from '@jupyterlab/application';
-
+import {JupyterLab, JupyterLabPlugin} from '@jupyterlab/application';
 import '../style/index.css';
 import {GlobusHome} from "./globus/home";
-
-import {
-    IFileBrowserFactory
-} from '@jupyterlab/filebrowser';
+import {IDocumentManager} from '@jupyterlab/docmanager';
+import {GlobusWidgetManager} from "./globus/widget_manager";
 
 export const GLOBUS_TRANSFER_API_URL: string = 'https://transfer.api.globusonline.org/v0.10';
 export const GLOBUS_AUTH_URL: string = 'https://auth.globus.org/v2/oauth2/authorize';
 export const GLOBUS_AUTH_TOKEN: string = 'https://auth.globus.org/v2/oauth2/token';
-
-export const LOADING_ICON = document.createElement('div');
-LOADING_ICON.className = 'loader';
-
-export const CHECKMARK_ICON = document.createElement('div');
-CHECKMARK_ICON.className = 'checkmark';
-
-export const CROSS_ICON = document.createElement('div');
-CROSS_ICON.className = 'cross';
 
 /**
  * Globus plugin
  */
 export const globus: JupyterLabPlugin<void> = {
     id: '@jupyterlab/globus:file-browser',
-    requires: [IFileBrowserFactory],
+    requires: [IDocumentManager],
     activate: activateGlobus,
     autoStart: true
 };
 
-function activateGlobus(app: JupyterLab) {
+function activateGlobus(app: JupyterLab, manager: IDocumentManager) {
     console.log('JupyterLab extension jupyterlab_globus is activated!');
 
-    let home: GlobusHome = new GlobusHome();
-
+    let widgetManager: GlobusWidgetManager = new GlobusWidgetManager(manager);
+    let home: GlobusHome = new GlobusHome(widgetManager);
     app.shell.addToLeftArea(home, {rank: 101});
 }
 
---
+++ src/utils.ts
@@ -0,0 +1,22 @@
+export const LOADING_ICON = document.createElement('div');
+LOADING_ICON.className = 'jp-Globus-loader';
+
+export const LOADING_LABEL = document.createElement('span');
+LOADING_LABEL.className = 'jp-Globus-loaderLabel';
+
+export const CHECKMARK_ICON = document.createElement('div');
+CHECKMARK_ICON.className = 'jp-Globus-checkmark';
+
+export const CROSS_ICON = document.createElement('div');
+CROSS_ICON.className = 'jp-Globus-cross';
+
+export const BG_IMAGES: any = {
+    'dir': 'var(--jp-Globus-icon-folder2)',
+    'file': 'var(--jp-Globus-icon-file)',
+};
+
+export function removeChildren(node: HTMLElement) {
+    while (node.firstChild) {
+        node.removeChild(node.firstChild);
+    }
+}
