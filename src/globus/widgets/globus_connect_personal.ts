7eb12c0133bdce251e9b54e2bfe0cebdb36c0904
Juan David Garrido <juandagarrido@hotmail.com>
2018-06-07 09:48:43 -0500

    GCP file browser implemented. HomeDir shown by default

--- src/globus/widgets/globus_connect_personal.ts
+++ src/globus/widgets/globus_connect_personal.ts
@@ -1,9 +1,9 @@
-import {IFileBrowserFactory} from "@jupyterlab/filebrowser";
+import {FileBrowser, IFileBrowserFactory} from "@jupyterlab/filebrowser";
 import {Widget, PanelLayout} from '@phosphor/widgets';
-import {ServerConnection} from '@jupyterlab/services';
+import {Drive, ServerConnection} from '@jupyterlab/services';
 import {} from 'node';
 import {IDocumentManager} from '@jupyterlab/docmanager';
-import {GCP_DRIVE_NAME, GCPDrive} from "../drive";
+import {URLExt} from '@jupyterlab/coreutils';
 
 const LOCALAPPDATA = 'AppData/Local/';
 const GCPCLIENTID = 'Globus Connect/log/globus_connect_personal.log';
@@ -12,23 +12,30 @@ const GLOBUS_CONNECT_PERSONAL = 'jp-Globus-connect-personal';
 
 export const CONNECT_PERSONAL = 'globus-connectPersonal';
 
+const SERVICE_DRIVE_URL = 'api/contents/';
+const GCP_DRIVE_NAME = 'GCPDrive';
+
 
 export class GlobusConnectPersonal extends Widget {
     private factory: IFileBrowserFactory;
+    private browser: FileBrowser;
+    private serverSettings: ServerConnection.ISettings;
+
 
     constructor(manager: IDocumentManager, factory: IFileBrowserFactory) {
         super();
         this.id = CONNECT_PERSONAL;
         this.layout = new PanelLayout();
         this.factory = factory;
+        this.serverSettings = ServerConnection.makeSettings();
         this.addClass(GLOBUS_CONNECT_PERSONAL);
 
         this.title.label = 'Globus Connect Personal';
 
         ServerConnection.makeRequest(
-            `http://localhost:8888/api/contents/${LOCALAPPDATA}${GCPCLIENTID}`,
+            `${this.serverSettings.baseUrl}${SERVICE_DRIVE_URL}${LOCALAPPDATA}${GCPCLIENTID}`,
             {},
-            ServerConnection.makeSettings())
+            this.serverSettings)
             .then(response => {return response.json()})
             .then(data => {
                 let path = '';
@@ -38,19 +45,43 @@ export class GlobusConnectPersonal extends Widget {
                         break;
                     }
                 }
-                const drive = new GCPDrive({apiEndpoint: path});
+                const drive = new Drive({name: GCP_DRIVE_NAME});
 
                 manager.services.contents.addDrive(drive);
                 console.log(drive);
-                this.createBrowser();
+
+                this.browser = this.factory.createFileBrowser('globus-connect-personal', {
+                    driveName: GCP_DRIVE_NAME
+                });
+
+                console.log('FINDING ENDPOINT...');
+                this.findGCPEndpoint(path)
+                    .then(path => {
+                        this.browser.model.cd(`/${path}`);
+                    });
+
+                (this.layout as PanelLayout).addWidget(this.browser);
             });
     }
 
-    private createBrowser(): void {
-        let browser = this.factory.createFileBrowser('globus-connect-personal', {
-            driveName: GCP_DRIVE_NAME
+    private async findGCPEndpoint(localPath: string | undefined) {
+        let baseUrl = this.serverSettings.baseUrl;
+        let url = URLExt.join(baseUrl, SERVICE_DRIVE_URL, localPath);
+
+        let promise = new Promise<string>((resolve) => {
+            ServerConnection.makeRequest(url, {}, this.serverSettings).then(response => {
+                if (response.status !== 200) {
+                    let parts = localPath.split('/');
+                    parts.shift();
+                    resolve(this.findGCPEndpoint(parts.join('/')));
+                }
+                else {
+                    console.log('GOT ENDPOINT: ' + localPath);
+                    resolve(localPath);
+                }
+            });
         });
 
-        (this.layout as PanelLayout).addWidget(browser);
+        return await promise;
     }
 }
