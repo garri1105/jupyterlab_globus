984f725ae08827fe13d1388d1d4410ed8141d525
Juan David Garrido <juandagarrido@hotmail.com>
2018-06-12 11:25:32 -0500

    Added error screen for user to restart jupyterLab and find GCP homedir

--- src/globus/widgets/globus_connect_personal.ts
+++ src/globus/widgets/globus_connect_personal.ts
@@ -4,8 +4,13 @@ import {Drive, ServerConnection} from '@jupyterlab/services';
 import {} from 'node';
 import {IDocumentManager} from '@jupyterlab/docmanager';
 import {JupyterLab} from "@jupyterlab/application";
+import {
+    ERROR_IMAGE, GLOBUS_BUTTON, GLOBUS_FLEX_CENTER_SCREEN, GLOBUS_BORDER, LOADING_ICON,
+    LOADING_LABEL
+} from "../../utils";
 
 const GLOBUS_CONNECT_PERSONAL = 'jp-Globus-connect-personal';
+const GLOBUS_ERROR_SCREEN = 'jp-GlobusConnectPersonal-errorScreen';
 
 const LOCAL_APPDATA = 'AppData/Local/';
 const GCP_CLIENT_ID = 'Globus Connect/client-id.txt';
@@ -23,6 +28,7 @@ export class GlobusConnectPersonal extends Widget {
     private browser: FileBrowser;
     private serverSettings: ServerConnection.ISettings;
     private app: JupyterLab;
+    private errorScreen: HTMLDivElement;
 
     constructor(app: JupyterLab, manager: IDocumentManager, factory: IFileBrowserFactory) {
         super();
@@ -44,7 +50,6 @@ export class GlobusConnectPersonal extends Widget {
             driveName: GCP_DRIVE_NAME
         });
 
-        console.log('Calling navigatetoGCP');
         this.navigateToGCPHomeDir();
     }
 
@@ -55,7 +60,6 @@ export class GlobusConnectPersonal extends Widget {
             this.serverSettings)
             .then(response => {return response.json()})
             .then(data => {
-                console.log('FINDING ENDPOINT...');
                 let path = this.findGCPHomeDirPath(data.content);
                 let gcpEndpoint = this.findGCPEndpoint(path);
                 if (gcpEndpoint) {
@@ -63,7 +67,8 @@ export class GlobusConnectPersonal extends Widget {
                     (this.layout as PanelLayout).addWidget(this.browser);
                 }
                 else {
-                    console.log('Reset JL');
+                    let newPath = this.findAlternatePath(path);
+                    this.createErrorScreen(newPath);
                 }
             });
     }
@@ -77,12 +82,8 @@ export class GlobusConnectPersonal extends Widget {
     }
 
     private findGCPEndpoint(localPath: string | undefined) {
-        localPath = 'C:/Users/Juan David Grrido/Documents/Programming/Anaconda';
-        console.log(localPath);
-        console.log(this.app.info.directories.serverRoot);
         if (localPath.indexOf(this.app.info.directories.serverRoot) === 0) {
-            localPath = localPath.slice(this.app.info.directories.serverRoot.length,);
-            console.log(localPath);
+            localPath = localPath.slice(this.app.info.directories.serverRoot.length);
             return localPath;
         }
         else {
@@ -99,5 +100,50 @@ export class GlobusConnectPersonal extends Widget {
             .then(data => {
                 GCP_ENDPOINT_ID = data.content;
             });
+    }
+
+    private restartJL(newPath: string) {
+        LOADING_LABEL.textContent = 'Opening in a new window...';
+        this.errorScreen.appendChild(LOADING_ICON);
+        this.errorScreen.appendChild(LOADING_LABEL);
+        this.app.serviceManager.terminals.startNew().then(session => {
+            session.send({ type: 'stdin', content: [`jupyter lab --NotebookApp.notebook_dir="${newPath}"\r`]});
+        });
+    }
+
+    private createErrorScreen(newPath: string) {
+        let errorText = document.createElement('div');
+        errorText.textContent = `Looks like your GCP home directory can\'t be accessed by your current JupyterLab 
+                                 startup location. Would you like to restart JupyterLab in "${newPath}" to fix this issue?`;
+        errorText.className = GLOBUS_BORDER;
+        errorText.style.padding = '5px 10px';
+
+        let restartJLButton = document.createElement('button');
+        restartJLButton.title = 'Restart JupyterLab';
+        restartJLButton.textContent = 'RESTART';
+        restartJLButton.className = `jp-mod-styled jp-mod-accept ${GLOBUS_BUTTON}`;
+        restartJLButton.addEventListener('click', () => this.restartJL(newPath));
+
+        this.errorScreen = document.createElement('div');
+        this.errorScreen.className = `${GLOBUS_FLEX_CENTER_SCREEN} ${GLOBUS_ERROR_SCREEN}`;
+        this.errorScreen.appendChild(ERROR_IMAGE);
+        this.errorScreen.appendChild(errorText);
+        this.errorScreen.appendChild(restartJLButton);
+
+        this.node.appendChild(this.errorScreen);
+    }
+
+    private findAlternatePath(path: string) {
+        let split1 = path.split('/');
+        let split2 = this.app.info.directories.serverRoot.split('/');
+
+        let i = 0;
+        let newPath = '';
+        while (i < split1.length && i < split2.length && split1[i] == split2[i]) {
+            newPath += `${split1[i]}/`;
+            i += 1;
+        }
+
+        return newPath ? newPath : `${split1[0]}/`;
     }
 }
