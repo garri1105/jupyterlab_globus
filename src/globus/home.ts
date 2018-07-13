64ade5bc452bf5cb2a7879b8eb5eb76d36eaadc9
Juan David Garrido <juandagarrido@hotmail.com>
2018-07-13 09:09:48 -0500

    Refactoring

--- src/globus/home.ts
+++ src/globus/home.ts
@@ -34,7 +34,7 @@ export class GlobusHome extends Widget {
         this.title.closable = true;
 
         this.showLoginScreen();
-        // this.globusLogin.attemptSignIn();
+        this.globusLogin.attemptSignIn();
     }
 
     public showLoginScreen() {
@@ -78,10 +78,10 @@ export class GlobusLogin extends Widget {
         oauth2SignIn();
     }
 
-    // attemptSignIn() {
-    //     let data = sessionStorage.getItem('data');
-    //     if (data) {
-    //         globusAuthorized.resolve(JSON.parse(data));
-    //     }
-    // }
+    attemptSignIn() {
+        let data = sessionStorage.getItem('data');
+        if (data) {
+            globusAuthorized.resolve(JSON.parse(data));
+        }
+    }
 }
